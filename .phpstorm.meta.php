977e11ae8fdffc21fd060e38ed1f22e5dfaa35f7
龍門渕透華 <swearl@163.com>
2021-04-11 15:22:09 +0800

    小更新

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -1,3 +1,10 @@
 <?php
 
 namespace PHPSTORM_META;
+
+override(\model(0), map([
+    'App\Models\DownloadDetailModel' => \App\Models\DownloadDetailModel::class,
+    'App\Models\DownloadModel' => \App\Models\DownloadModel::class,
+    'App\Models\RadioModel' => \App\Models\RadioModel::class,
+    'App\Models\VideoModel' => \App\Models\VideoModel::class,
+]));
